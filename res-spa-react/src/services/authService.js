6e6dd6bb20285a1cdcfe925c25d5290328d57c72
LJ Parfan <ljparfan22@gmail.com>
2018-11-30 22:21:49 +0800

    updated apiLink

--- res-spa-react/src/services/authService.js
+++ res-spa-react/src/services/authService.js
@@ -1,7 +1,7 @@
 import http from "./httpService";
 // import config from "config";
 
-const api = "http://localhost:5000";
+const api = "http://realestateseller-api.herokuapp.com/";
 
 function login({ username, password }) {
    return http.post(`${api}/api/auth`, { username, password });
