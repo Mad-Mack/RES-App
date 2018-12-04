13a004999d4d5899b60b9eda0c8481c94addeb1c
Leonard Parfan <LeonardParfan@Deltek.com>
2018-12-04 17:04:27 +0800

    added asp.net

---
+++ res_api_aspnetcore/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using res_api_aspnetcore.Models;
+using System.Threading.Tasks;
+
+namespace res_api_aspnetcore.Services
+{
+    public interface IUserService
+    {
+        Task<ApplicationUser> Register(ApplicationUser user);
+        Task<bool> UserExists(string identifier);
+        Task<ApplicationUser> GetCurrentUser(string username);
+        string GenerateAuthToken(ApplicationUser user);
+    }
+}
---
+++ res_api_aspnetcore/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using res_api_aspnetcore.Models;
+
+namespace res_api_aspnetcore.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(UserManager<ApplicationUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+        public string GenerateAuthToken(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUser> GetCurrentUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ApplicationUser> Register(ApplicationUser user, string password)
+        {
+            var result = _userManager.CreateAsync(user, password);
+        }
+
+        public Task<bool> UserExists(string identifier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
