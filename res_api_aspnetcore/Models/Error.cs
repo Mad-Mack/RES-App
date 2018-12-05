fdbd2b027408eae955165ad0a1b721aced31ef92
Leonard Parfan <LeonardParfan@Deltek.com>
2018-12-05 16:36:37 +0800

    delte aspnetcore

--- res_api_aspnetcore/Models/Error.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace res_api_aspnetcore.Models
-{
-    public class Error
-    {
-        public List<string> Errors { get; set; } = new List<string>();
-    }
-}
